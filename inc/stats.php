990a792ca61f338b93df2fdb0b168226444739e6
Tom Brooks <tom@olospo.co.uk>
2023-03-30 10:22:05 +0000

    Home and About updates

--- inc/stats.php
+++ inc/stats.php
@@ -1,19 +1,19 @@
 <section class="stat_section">
   <div class="container">
-    <div class="stats">
-      <div class="stat three columns grey">
+    <div class="stats four">
+      <div class="stat grey">
         <span class="unit">22</span>
         <span class="description">Years</span>
       </div>
-      <div class="stat three columns green">
+      <div class="stat green">
         <span class="unit">200+</span>
         <span class="description">Firms engaged each year</span>
       </div>
-      <div class="stat three columns grey">
+      <div class="stat grey">
         <span class="unit">50+</span>
         <span class="description">Benchmarks</span>
       </div>
-      <div class="stat three columns white">
+      <div class="stat white">
         <span class="unit">5.1TB</span>
         <span class="description">Mapped Client Data</span>
       </div> 
--- templates/about.php
+++ templates/about.php
@@ -1,103 +1,92 @@
 <?php /* Template Name: About */
 get_header();
 
-$heroBG = get_field('hero_background_image');
-
 while ( have_posts() ) : the_post(); ?>
 
-<section class="hero about" style="background: url('<?php echo esc_url($heroBG['url']); ?>') center center no-repeat; background-size: cover;">
-  <div class="overlay"></div>
-  <div class="logo">
-    <img src="<?php bloginfo('template_directory'); ?>/img/logo_large.svg" alt="AP Logo" />
-  </div>
-</section>
+<?php get_template_part( 'inc/hero' ); ?>
 
-<section class="intro about">
-  <div class="container">
-    <div class="content ten columns offset-by-one">
-      <?php the_field('intro_content'); ?>
-    </div>
-  </div>
-</section>
+<?php get_template_part( 'inc/text' ); ?>
 
-<section class="studio about" id="studio">
+<section class="square_section green">
   <div class="container">
-    <div class="content ten columns offset-by-one">
-    <h2><?php the_field('studio_title'); ?></h2>
-    <?php the_field('studio_content'); ?>
-    </div>
-  </div>
-</section>
-
-<section class="clients about">
-  <div class="container">
-    <div class="content ten columns offset-by-one">
-    <h2>Clients</h2>
-    <?php if( have_rows('client') ): ?>
-        <div class="row">
-        <?php while( have_rows('client') ): the_row(); 
-            $image = get_sub_field('logo');
-            ?>
-            <article class="logo three columns">
-              <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" loading="lazy" />
-            </article>
-        <?php endwhile; ?>
+    <div class="square_content">
+      <div class="content">
+        <div class="title">
+          <h3 class="split_title">Our Experience</h3>
         </div>
-    <?php endif; ?>
-
-    </div>
-    </div>
-  </div>
-</section>
-
-<?php get_template_part( 'inc/cta_contact' ); ?>
-
-<section class="process about">
-  <div class="container">
-    <div class="eight columns offset-by-two">
-      <h2><?php the_field('process_title'); ?></h2>
-      <?php if( have_rows('process') ): ?>
-        <?php while( have_rows('process') ): the_row(); 
-            $title = get_sub_field('process_title');
-            $desc = get_sub_field('process_description');
-          ?>
-          <article class="step">
-            <div class="shapes">
-              <img src="<?php bloginfo('template_directory'); ?>/img/<?php echo $title; ?>_shapes.svg" class="<?php echo $title; ?>"/>
-            </div>
-            <div class="content">
-              <h3><?php echo $title; ?></h3>
-              <p><?php echo $desc; ?></p>
-            </div>
-          </article>
-        <?php endwhile; ?>
-      <?php endif; ?>
-    </div>
-  </div>
-</section>
-
-<section class="team about" id="team">
-  <div class="container">
-    <div class="content ten columns offset-by-one">
-      <h2>Meet the team</h2>
-      
-      <?php if (have_rows('team_profile')) { // Flexible Content ?>
-      <div class="flexible_content flex">        
-        <?php while (have_rows('team_profile')) { the_row(); ?>
-          <?php if( get_row_layout() == 'profile_layout' ): ?>
-            <?php get_template_part( 'inc/profile' ); ?>
-          <?php elseif( get_row_layout() == 'heading' ): ?>
-            <h3 class="team_heading"><?php the_sub_field('heading'); ?></h3>
-          <?php endif; ?>
-        <?php } ?>
+        <p>Each delivery team provides unrivalled levels of industry experience in financial markets, operations and technology services. We offer a unique perspective on solving problems associated with researching, sourcing and implementing new and innovative solutions for financial markets, leveraging years of experience to ensure unparalleled accuracy while maintaining a low burden on participants.</p>
       </div>
-      <?php } ?>
-
+    </div>
+    <div class="square_background">
+    
     </div>
   </div>
 </section>
 
-<?php get_template_part( 'inc/cta_careers' ); ?>
+<section class="stat_section">
+  <div class="container">
+    <div class="stats three">
+      <div class="stat grey">
+        <span class="unit">~350</span>
+        <span class="description">Participants attended each year</span>
+      </div>
+      <div class="stat white">
+        <span class="unit">~30</span>
+        <span class="description">Roundtables held annually</span>
+      </div>
+      <div class="stat grey">
+        <span class="unit">~200</span>
+        <span class="description">Firms represented each year</span>
+      </div> 
+    </div>
+  </div>
+</section>
+
+<section class="our_products">
+  <div class="container">
+    <div class="title six columns">
+      <h3 class="split_title">Our Values</h3>
+      <p>Expand embraces diversity, innovation and creativity and recognizes values and core competencies that apply to everyone. These values underpin the very work that we do.</p>
+    </div>
+    <div class="row">
+      <article class="one-third column">
+        <div class="content">
+          <h4>Integrity</h4>
+          <p>Int et quia nullab id even im fugiae mi, sit fugit que nonem ipsu nti busam, sum facium ullest, to cus pore com molor rem volup</p>
+        </div>
+      </article>
+      <article class="one-third column">
+        <div class="content">
+          <h4>Respect for the individual</h4>
+          <p>Int et quia nullab id even im fugiae mi, sit fugit que nonem ipsu nti busam, sum facium ullest, to cus pore com molor rem volup</p>
+        </div>
+      </article>
+      <article class="one-third column">
+        <div class="content">
+          <h4>Ambassador of the firm</h4>
+          <p>Int et quia nullab id even im fugiae mi, sit fugit que nonem ipsu nti busam, sum facium ullest, to cus pore com molor rem volup</p>
+        </div>
+      </article>
+    </div>
+  </div>
+</section>
+
+<section class="careers_cta">
+  <div class="container">
+    <div class="cta_background">
+      
+    </div>
+    <div class="cta_content">
+      <div class="content">
+      <h4>We're building tomorrow</h4>
+      <p>Flourish in an environment where creative thinking is encouraged. Become a part of our unique, diverse and entrepreneurial environment</p>
+      <p><a href="#" class="button">Apply now</a><br />
+      <a href="#" class="button">Explore our culture</a></p>
+      </div>
+    </div>
+  </div>
+</section>
+
 
 <?php endwhile; // end of the loop. ?>
 
