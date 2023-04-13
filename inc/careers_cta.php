4ae13df4a2dfb9f8898b2e5b4afa00c86a62c1cd
Tom Brooks <tom@olospo.co.uk>
2023-04-13 14:25:00 +0000

    Careers fix

---
+++ inc/careers_cta.php
@@ -0,0 +1,17 @@
+<section class="careers_cta">
+  <div class="container">
+    <div class="cta_background">
+      <div class="cta_title">
+        <img src="<?php bloginfo('template_directory'); ?>/img/expand_your_world.svg" alt="Expand Your World">
+      </div>
+    </div>
+    <div class="cta_content">
+      <div class="content">
+      <h4>We're building tomorrow</h4>
+      <p>Flourish in an environment where creative thinking is encouraged. Become a part of our unique, diverse and entrepreneurial environment</p>
+      <p><a href="#" class="button">Apply now</a><br />
+      <a href="#" class="button">Explore our culture</a></p>
+      </div>
+    </div>
+  </div>
+</section>
