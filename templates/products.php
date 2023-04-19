0e114918cd351054beaac36fca00157ddc5ccc0c
Tom Brooks <tom@olospo.co.uk>
2023-04-19 09:22:29 +0000

    Formatting update

--- templates/products.php
+++ templates/products.php
@@ -33,18 +33,34 @@ while ( have_posts() ) : the_post(); ?>
       <button class="w3-bar-item w3-button tablink" onclick="openTab(event,'Normalisation')">Normalisation</button>
       <button class="w3-bar-item w3-button tablink" onclick="openTab(event,'Actionable')">Actionable Insights</button>
     </div> 
-    <article id="Data" class="tab twelve columns">
-      <p>Expand collects raw data from clients rather than relying on filling in complex templates. This not only reduces the effort needed to participate, but also allows us to ensure database integrity as well as alignment in scope and definitions.</p>
+    <article id="Data" class="tab eight columns offset-by-two">
+      <div class="contain">
+        <div class="content">
+          <p>Expand collects raw data from clients rather than relying on filling in complex templates. This not only reduces the effort needed to participate, but also allows us to ensure database integrity as well as alignment in scope and definitions.</p>
+        </div>
+      </div>
     </article>
     
-    <article id="Data Mapping" class="tab twelve columns" style="display:none">
-      <p>Benchmark data is collected via MIS extracts, is reviewed, and mapped by Expand analysts to a standard taxonomy and verified by the client. This ensures minimal work for participants while allowing for the strongest like-for-like comparisons</p>
+    <article id="Data Mapping" class="tab eight columns offset-by-two" style="display:none">
+      <div class="contain">
+        <div class="content">
+        <p>Benchmark data is collected via MIS extracts, is reviewed, and mapped by Expand analysts to a standard taxonomy and verified by the client. This ensures minimal work for participants while allowing for the strongest like-for-like comparisons</p>
+        </div>
+      </div>
     </article>
-    <article id="Normalisation" class="tab twelve columns" style="display:none">
-      <p>Expand leverages years of experience to ensure unparalleled relevancy of comparisons. Peer groups can be tailored, and other complexity and scaling measures are captured and verified to create truly bespoke indicators of performance.</p>
+    <article id="Normalisation" class="tab eight columns offset-by-two" style="display:none">
+      <div class="contain">
+        <div class="content">
+        <p>Expand leverages years of experience to ensure unparalleled relevancy of comparisons. Peer groups can be tailored, and other complexity and scaling measures are captured and verified to create truly bespoke indicators of performance.</p>
+        </div>
+      </div>
     </article>
-    <article id="Actionable" class="tab twelve columns" style="display:none">
-      <p>Expand’s unique insights provide senior stakeholders and decision makers with clear and actionable intelligence about competitiveness, performance, and opportunities. Helping clients make strategic, well-thought-out decisions to drive positive business outcomes. </p>
+    <article id="Actionable" class="tab eight columns offset-by-two" style="display:none">
+      <div class="contain">
+        <div class="content">
+        <p>Expand’s unique insights provide senior stakeholders and decision makers with clear and actionable intelligence about competitiveness, performance, and opportunities. Helping clients make strategic, well-thought-out decisions to drive positive business outcomes. </p>
+        </div>
+      </div>
     </article>
   </div>
 </section>
