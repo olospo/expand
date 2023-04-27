55944b2daa8d9eedbafe2612d46c0f0e333313c4
Tom Brooks <tom@olospo.co.uk>
2023-04-27 13:18:56 +0000

    Profile updates

--- inc/profile.php
+++ inc/profile.php
@@ -5,6 +5,7 @@ setup_postdata($post); ?>
 <article class="team_member">
   <div class="image" style="background: url(<?php the_post_thumbnail_url( 'large-thumb' ); ?>) top center no-repeat; background-size: cover;"></div>
   <p><strong><?php the_title(); ?></strong><br />
-  <?php the_field('job_title'); ?></p>
+  <?php the_field('job_title'); ?><br />
+  <?php the_field('location'); ?></p>
 </article>
 <?php wp_reset_postdata(); ?>
