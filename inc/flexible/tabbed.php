7f41684001f886b8d5f7bd0fd0e2525fe6bcb150
Tom Brooks <tom@olospo.co.uk>
2023-10-04 18:11:48 +0100

    Tabbed title

--- inc/flexible/tabbed.php
+++ inc/flexible/tabbed.php
@@ -1,48 +1,47 @@
-<?php 
-if (have_rows('tab')): ?>
-  <section class="tabbed_section">
-    <div class="container">
-      <div class="title twelve columns">
-        <h3 class="split_title">Our Unique Approach</h3>
-      </div>
-
-      <?php 
-      $counter = 0;
-      $buttons = '';
-      $articles = '';
-      while (have_rows('tab')): the_row(); 
-        $title = get_sub_field('title');
-        $content = get_sub_field('content');
-        $icon = get_sub_field('icon');
-
-        $active_class = ($counter == 0) ? 'active' : '';
-        $display_style = ($counter == 0) ? '' : 'display:none';
-        
-        $buttons .= '<button class="w3-bar-item w3-button tablink ' . $active_class . '" onclick="openTab(event,\'' . $title . '\')">' . $title . '</button>';
-        
-        $articles .= '<article id="' . $title . '" class="tab ten columns offset-by-one" style="' . $display_style . '">
-                          <div class="contain">';
-
-        if (!empty($icon)) {
-          $url = $icon['url'];
-          $alt = $icon['alt'];
-          $articles .= '<div class="icon"><img src="' . $url . '" alt="'. $alt . '" /></div>';
-        }
-
-        $articles .= '<div class="content">' . $content . '</div>
-                      </div>
-                    </article>';
-
-        $counter++;
-      endwhile;
-      ?>
-
-      <div class="tab_menu twelve columns <?php echo $word; ?>">
-        <?php echo $buttons; ?>
-      </div> 
-
-      <?php echo $articles; ?>
-
+<section class="tabbed_section">
+  <div class="container">
+    <div class="title twelve columns">
+      <h3 class="split_title"><?php the_sub_field('title'); ?></h3>
     </div>
-  </section>
-<?php endif; ?>
+    <?php 
+    if (have_rows('tab')): ?>
+    <?php 
+    $counter = 0;
+    $buttons = '';
+    $articles = '';
+    while (have_rows('tab')): the_row(); 
+      $title = get_sub_field('title');
+      $content = get_sub_field('content');
+      $icon = get_sub_field('icon');
+
+      $active_class = ($counter == 0) ? 'active' : '';
+      $display_style = ($counter == 0) ? '' : 'display:none';
+      
+      $buttons .= '<button class="w3-bar-item w3-button tablink ' . $active_class . '" onclick="openTab(event,\'' . $title . '\')">' . $title . '</button>';
+      
+      $articles .= '<article id="' . $title . '" class="tab ten columns offset-by-one" style="' . $display_style . '">
+                        <div class="contain">';
+
+      if (!empty($icon)) {
+        $url = $icon['url'];
+        $alt = $icon['alt'];
+        $articles .= '<div class="icon"><img src="' . $url . '" alt="'. $alt . '" /></div>';
+      }
+
+      $articles .= '<div class="content">' . $content . '</div>
+                    </div>
+                  </article>';
+
+      $counter++;
+    endwhile;
+    ?>
+
+    <div class="tab_menu twelve columns <?php echo $word; ?>">
+      <?php echo $buttons; ?>
+    </div> 
+
+    <?php echo $articles; ?>
+
+  </div>
+  <?php endif; ?>
+</section>
