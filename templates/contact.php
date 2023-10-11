ac7e0a02924ad187e7413c2493af5d0d682afd45
Tom Brooks <tom@olospo.co.uk>
2023-10-11 13:54:22 +0100

    Contact update

--- templates/contact.php
+++ templates/contact.php
@@ -3,17 +3,30 @@ get_header();
 
 while ( have_posts() ) : the_post(); ?>
 
-<section class="square_section green">
-  <div class="container">
-    <div class="square_content">
-      <div class="content">
-        <div class="title">
-          <h3 class="split_title"><?php the_field('square_title'); ?></h3>
-        </div>
-        <?php the_field('square_content'); ?>
-      </div>
-    </div>
-    <div class="square_background" style="background: url('<?php the_field('square_image'); ?>') center center no-repeat; background-size: cover;"></div>
+<?php // Hero
+  $type = get_field('video_or_image');
+  $opacity = get_field('opacity_overlay');
+  $image = get_field('video_poster');
+  $videomp4 = get_field('upload_video_mp4');
+  $videowebm = get_field('upload_video_webm');
+  $bgImage = get_field('background_image');
+  $icon = get_field('icon');
+  $positionIcon = get_field('icon_position');
+?>
+
+<section class="hero home">
+  <div class="video-upload" <?php if ($type == "image") { ?> style="background: linear-gradient(rgba(0, 0, 0, 0.<?php echo $opacity; ?>), rgba(0, 0, 0, 0.<?php echo $opacity; ?>)), url('<?php echo $bgImage; ?>') center center no-repeat; background-size:cover;" <?php } elseif ($type == "video") { ?> style="background: linear-gradient(rgba(0, 0, 0, 0.<?php echo $opacity; ?>), rgba(0, 0, 0, 0.<?php echo $opacity; ?>))" <?php } ?>>
+    <?php if ($type == "video") { ?>
+    <video data-object-fit="cover" playsinline muted autoplay loop  id="homeVideo" poster="<?php echo $image['url']; ?>">
+      <source src="<?php echo $videowebm; ?>" type="video/webm">
+      <source src="<?php echo $videomp4; ?>" type="video/mp4">
+    </video>
+    <div class="poster" style="background: url('<?php echo $image['url']; ?>') center center no-repeat; background-size: cover"></div>
+    <?php } ?>
+    
+    <?php if ($icon) { ?>  
+    <div class="icon <?php echo $positionIcon; ?>"><h1><img src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($icon['alt']); ?>"></h1></div>
+    <?php } ?>
   </div>
 </section>
 
