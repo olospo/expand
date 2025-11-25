3ecafdf9a9fa6fa2127941cefa9fbcb214225904
Tom Brooks <tom@olospo.co.uk>
2025-11-25 12:29:07 +0000

    Solutions

--- functions.php
+++ functions.php
@@ -289,7 +289,7 @@ function custom_post_type() {
 	);
 	register_post_type( 'profile', $args );	
 	
-	// Profiles Post Type
+	// Products Post Type
 	$labels = array(
 		'name'                => _x( 'Products', 'Post Type General Name', 'text_domain' ),
 		'singular_name'       => _x( 'Product', 'Post Type Singular Name', 'text_domain' ),
@@ -331,6 +331,50 @@ function custom_post_type() {
 		],
 	);
 	register_post_type( 'product', $args );	
+	
+	// Products Post Type
+	$labels = array(
+		'name'                => _x( 'Solutions', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Solution', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Solutions', 'text_domain' ),
+		'all_items'           => __( 'All Solutions', 'text_domain' ),
+		'view_item'           => __( 'View Solution', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Solution', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'edit_item'           => __( 'Edit Solution', 'text_domain' ),
+		'update_item'         => __( 'Update Solution', 'text_domain' ),
+		'search_items'        => __( 'Search Solutions', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$args = array(
+		'label'               => __( 'Solution', 'text_domain' ),
+		'description'         => __( 'Solutions', 'text_domain' ),
+		'labels'              => $labels,
+		'supports' => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom-fields', 'page-attributes' ),
+		'hierarchical'        => true,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 28,
+		'menu_icon'           => 'dashicons-analytics',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'template'     => [],
+		'template_lock' => false,
+		'map_meta_cap'        => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+		'rewrite' => [
+			'slug' => 'solution',
+			'with_front' => false
+		],
+	);
+	register_post_type( 'solution', $args );	
+	
 }
 // Hook into the 'init' action
 add_action( 'init', 'custom_post_type', 0 );
@@ -541,7 +585,7 @@ function number_to_word($number) {
 
 /**
  * Add a "Post Depth" location rule for hierarchical post types.
- */
+*/
 
 // 1. Register rule type
 add_filter('acf/location/rule_types', function ($choices) {
