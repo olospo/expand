05b34092b329849f378c8031bd7182c4a889e469
Tom Brooks <tom@olospo.co.uk>
2026-05-13 10:07:54 +0100

    v1.0.1 - Gradient text options added

--- inc/flexible/text.php
+++ inc/flexible/text.php
@@ -1,14 +1,17 @@
 <?php 
+$color = get_sub_field('colour_scheme');
 $title = get_sub_field('title');
+$custom = get_sub_field('custom_title_split');
+$split = get_sub_field('split_title');
 $text = get_sub_field('gradient_text');
 ?>
 
-<section class="text_section">
+<section class="text_section <?php echo $color; ?>">
   <div class="container">
     <div class="ten columns offset-by-one">
       <?php if ($title) { ?>  
       <div class="title">
-        <h3 class="split_title"><?php echo $title; ?></h3>
+        <h3 class="split_title <?php if( $custom ) { echo $split; } ?>"><?php echo $title; ?></h3>
       </div>
       <?php } ?>
       <?php echo $text; ?>
